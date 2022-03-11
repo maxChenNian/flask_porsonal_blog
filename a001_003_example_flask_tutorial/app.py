c83a6830d1268a7bd60809206811dcbe4f3693d6
Liyang Fang <1091403476@qq.com>
2022-03-11 10:01:54 +0800

    fang: finish a watchlist web and test. 20220311

--- a001_003_example_flask_tutorial/app.py
+++ a001_003_example_flask_tutorial/app.py
@@ -33,14 +33,14 @@ class Movie(db.Model):  # 表名将会是 movie
 import click
 
 
-@app.cli.command()  # 注册为命令
-@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
-def initdb(drop):
-    """Initialize the database."""
-    if drop:  # 判断是否输入了选项
-        db.drop_all()
-    db.create_all()
-    click.echo('Initialized database.')  # 输出提示信息
+# @app.cli.command()  # 注册为命令
+# @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+# def initdb(drop):
+#     """Initialize the database."""
+#     if drop:  # 判断是否输入了选项
+#         db.drop_all()
+#     db.create_all()
+#     click.echo('Initialized database.')  # 输出提示信息
 
 
 # -----------------------------------------------------------------------------
@@ -152,7 +152,7 @@ def settings():
             flash('Invalid input.')
             return redirect(url_for('settings'))
 
-        current_user.name = name
+        current_user.username = name
         # current_user 会返回当前登录用户的数据库记录对象
         # 等同于下面的用法
         # user = User.query.first()
@@ -246,3 +246,44 @@ def delete(movie_id):
     db.session.commit()  # 提交数据库会话
     flash('Item deleted.')
     return redirect(url_for('index'))  # 重定向回主页
+
+
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+
+# 生成虚拟数据
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Liyang Fang'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
